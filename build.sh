735b374bd43a7de42e976397ad728b134b468102
alex <cyy0523xc@gmail.com>
2018-06-22 10:42:23 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@
 # Created Time: 2018年06月22日 星期五 10时37分12秒
 user=`whoami`
 sudo -H pyinstaller -w -F -p . --clean es2csv_cli.py
-sudo $user:$user ./dist/es2csv_cli
+sudo chown $user:$user ./dist
 mv ./dist/es2csv_cli ./dist/es2csv
